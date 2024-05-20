f702554cf181857e6e1534c8ee79414868c90615
santiago <santiagolopezzz.993@gmail.com>
2024-05-20 00:43:31 -0300

    se realiza el uml del ej8 de la guia 2 se realizo el paso de uml a Java

---
+++ guia2/src/guia2/ej8/Droide.java
@@ -0,0 +1,31 @@
+package guia2.ej8;
+
+import java.util.ArrayList;
+
+/**
+ * Código generado por la app UXFtoJava by Charly Cimino
+ *
+ * @see https://github.com/CharlyCimino/uxf-to-java
+ */
+public class Droide {
+
+    ArrayList<Pieza> listaPiezaNoOperativos;
+    private ArrayList<Pieza> listaPiezasOperativos;
+    private ArrayList<Droide> droidesRotos;
+
+    public ResultadoReparacion autoRepararse() {
+        // Método a resolver...
+        return null;
+    }
+
+    private ResultadoReparacion repararme() {
+        // Método a resolver...
+        return null;
+    }
+
+    private int efectuarReparacion() {
+        // Método a resolver...
+        return 0;
+    }
+
+}
---
+++ guia2/src/guia2/ej8/Pieza.java
@@ -0,0 +1,21 @@
+package guia2.ej8;
+
+/**
+ * Código generado por la app UXFtoJava by Charly Cimino
+ *
+ * @see https://github.com/CharlyCimino/uxf-to-java
+ */
+public class Pieza {
+
+    private String nombre;
+
+    public void Habitacion(int num, double precio) {
+        // Método a resolver...
+    }
+
+    public double calcularTotal() {
+        // Método a resolver...
+        return 0;
+    }
+
+}
