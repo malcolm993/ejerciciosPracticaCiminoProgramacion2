52b8c2ffbb4982aabfa922216ad91be99b26d2d6
santiago <santiagolopezzz.993@gmail.com>
2024-05-26 23:58:33 -0300

    nueva carpeta guia 3 ej 2

---
+++ Guia3/src/guia3/ej2/Archivo.java
@@ -0,0 +1,34 @@
+package guia3.ej2;
+
+/**
+ * Código generado por la app UXFtoJava by Charly Cimino
+ *
+ * @see https://github.com/CharlyCimino/uxf-to-java
+ */
+public class Archivo {
+
+    private String nombre;
+    private int peso;
+    private String localizacion;
+    private boolean isAbierto;
+
+    public Archivo(String string1, int int2, String string3, boolean boolean4) {
+        // Constructor a resolver...
+    }
+
+    public boolean abrir() {
+        // Método a resolver...
+        return false;
+    }
+
+    public boolean cerrar() {
+        // Método a resolver...
+        return false;
+    }
+
+    public String moverUbicacion() {
+        // Método a resolver...
+        return "";
+    }
+
+}
---
+++ Guia3/src/guia3/ej2/ArchivoAudio.java
@@ -0,0 +1,18 @@
+package guia3.ej2;
+
+/**
+ * Código generado por la app UXFtoJava by Charly Cimino
+ *
+ * @see https://github.com/CharlyCimino/uxf-to-java
+ */
+public class ArchivoAudio extends ArchivoMultimedia {
+
+    private String artista;
+    private String album;
+    private FormatoAudio tipoFormatoAudio;
+
+    public ArchivoAudio(String string1, int int2, String string3, boolean boolean4, int int5, boolean boolean6, String string7, String string8) {
+        super(string1, int2, string3, boolean4, int5, boolean6);
+    }
+
+}
